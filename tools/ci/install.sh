55e3a307af7100774a81b6f267f8d892e5d2c5b9
Boris Kaul <localvoid@gmail.com>
2016-05-08 13:25:37 +0600

    Add missing dev dependencies

--- tools/ci/install.sh
+++ tools/ci/install.sh
@@ -5,7 +5,7 @@ set -ex -o pipefail
 echo 'travis_fold:start:INSTALL'
 
 echo 'travis_fold:start:install.npm-cli-tools'
-npm -g install gulp-cli typings
+npm -g install gulp-cli typings karma-cli
 echo 'travis_fold:end:install.npm-cli-tools'
 
 echo 'travis_fold:start:install.node_modules'
