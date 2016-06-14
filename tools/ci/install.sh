e7ce96539fbd81d52b7c362fd56e3a45160df93f
Boris Kaul <localvoid@gmail.com>
2016-06-14 09:25:45 +0600

    Remove typings from ci script

--- tools/ci/install.sh
+++ tools/ci/install.sh
@@ -5,15 +5,11 @@ set -ex -o pipefail
 echo 'travis_fold:start:INSTALL'
 
 echo 'travis_fold:start:install.npm-cli-tools'
-npm -g install gulp-cli typings karma-cli tslint
+npm -g install gulp-cli karma-cli tslint
 echo 'travis_fold:end:install.npm-cli-tools'
 
 echo 'travis_fold:start:install.node_modules'
 npm install
 echo 'travis_fold:end:install.node_modules'
-
-echo 'travis_fold:start:install.typings'
-typings install
-echo 'travis_fold:end:install.typings'
 
 echo 'travis_fold:end:INSTALL'
